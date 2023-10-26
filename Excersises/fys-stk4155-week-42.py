61a919d2298801a598eaf6f6dc1f3dca1356fe95
Janita Willumsen <j.willu@me.com>
2023-10-26 12:50:09 +0200

    Update SDG

--- Excersises/fys-stk4155-week-42.py
+++ Excersises/fys-stk4155-week-42.py
@@ -11,9 +11,9 @@ class StochasticGradientDecent:
     def __init__(self, n, p):
         self.n = n
         self.p = p
-        self.x = 2*np.random.rand(n, 1)
-        self.y = 4 + 3*self.x + np.random.randn(n, 1)
-        self.X = None
+        self._x = np.linspace(0, 1, n).reshape(-1, 1)
+        self._y = 4 + 3*self._x + np.random.randn(n, 1)
+        self._X = None
         self._eig = None
         self._theta = None
         self._predict = None
@@ -21,23 +21,31 @@ class StochasticGradientDecent:
         # self.y = input[1]
 
 
+    def get_x(self):
+        return self._x
+    
+
+    def get_y(self):
+        return self._y
+    
+
     def eta(self):
-        xtx = self.X.T @ self.X
+        xtx = self._X.T @ self._X
         hessian = (2/self.n) * xtx
         self.eig, _ = np.linalg.eig(hessian)
-        self.eta = 1/np.max(self.eig)
+        self._eta = 1/np.max(self.eig)
 
 
     def design_matrix(self):
-        x = np.ravel(self.x)
+        x = np.ravel(self._x)
         n = len(x)
         if self.p >= 1:
             X = np.ones((n, self.p))
             for i in range(1, self.p):
                 X[:, i] = x**i
-            self.X = X
+            self._X = X
         else:
-            self.X = np.ones((n, 1))
+            self._X = np.ones((n, 1))
 
 
 
@@ -45,32 +53,42 @@ class StochasticGradientDecent:
         return t0 / (t+t1)
 
     
-    def theta(self, n_epoch, m_batch, t0, t1):
-        m_size = int(self.n / m_batch)
+    def theta(self, n_epoch, batch_size, t0, t1):
+        n_batch = int(self.n / batch_size)
         theta = np.random.randn(self.p, 1)
 
         for e in range(n_epoch):
-            for i in range(m_size):
-                rand_idx = m_batch * np.random.randint(m_size)
-                Xi = self.X[rand_idx: rand_idx+m_batch]
-                yi = self.y[rand_idx: rand_idx+m_batch]
-                gradients = (2/m_batch) * Xi.T @ ((Xi @ theta) - yi)
-                t = e*m_size + i
+            for i in range(n_batch):
+                rand_idx = batch_size * np.random.randint(n_batch)
+                Xi = self._X[rand_idx: rand_idx+batch_size]
+                yi = self._y[rand_idx: rand_idx+batch_size]
+                gradients = (2/batch_size) * Xi.T @ ((Xi @ theta) - yi)
+                t = e*n_batch + i
                 eta = self.learning_schedule(t, t0, t1)
                 theta = theta - eta*gradients
-        self.theta = theta
+        self._theta = theta
 
     
     def solve(self):
-        x_test = np.random.rand(self.n, self.p)
-        self.predict = x_test @ self.theta
-        return self.predict
+        X_new = np.random.rand(self.n, self.p)
+        self._predict = X_new.dot(self._theta)
+        return self._predict
     
 
-
-if __name__ == '__main__':
+def main():
     model = StochasticGradientDecent(100, 2)
     model.design_matrix()
     model.theta(50, 5, 5, 50)
     y_predict = model.solve()
-    print(y_predict)
+
+    x = model.get_x()
+    y_true = model.get_y()
+
+    fig, ax = plt.subplots()
+    ax.scatter(x, y_true, color="black")
+    ax.scatter(x, y_predict, color="seagreen")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
