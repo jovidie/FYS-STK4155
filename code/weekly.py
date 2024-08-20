aeedaceca75fdebb6698829f9d0552a35d1bbd15
Janita Willumsen <janitaws@icloud.com>
2024-08-20 12:07:51 +0200

    Solve exercise 2

---
+++ code/weekly.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+
+def mse(y_true, y_pred):
+    n = np.size(y_pred)
+    return np.sum((y_true - y_pred)**2) / n
+
+def r2(y_true, y_pred):
+    num = np.sum((y_true - y_pred)**2)
+    denom = np.sum((y_true - np.mean(y_true))**2)
+    return 1 - (num / denom)
+
+def exercise_34_2():
+    x = np.random.rand(100)
+    y = 2.0 + 5*x**2 + 0.1*np.random.randn(100)
+
+    X = np.zeros((len(x), 3))
+    X[:, 0] = 1
+    X[:, 1] = x 
+    X[:, 2] = x*x 
+
+    beta = np.linalg.inv(X.T @ X) @ X.T @ y 
+    y_pred = X @ beta
+
+    poly = PolynomialFeatures(degree=2)
+    X_model = poly.fit_transform(x[:, np.newaxis])
+    model = LinearRegression()
+    model.fit(X, y)
+    model_pred = model.predict(X_model)
+
+    mse_man = mean_squared_error(y, y_pred)
+    r2_man = r2_score(y, y_pred)
+    mse_sk = mean_squared_error(y, model_pred)
+    r2_sk = r2_score(y, model_pred)
+    
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'bo')
+    ax.plot(x, y_pred, 'rx', label=f"MSE = {mse_man:.4f}, R2 = {r2_man:.4f}")
+    ax.plot(x, model_pred, 'g.', label=f"MSE = {mse_sk:.4f}, R2 = {r2_sk:.4f}")
+    ax.legend()
+    plt.show()
+
+
+def exercise_34_3():
+    n = 100
+    
+    x = np.linspace(-3, 3, n)
+    x = x.reshape(-1, 1)
+    noise = np.random.normal(0, 0.1, x.shape)
+    y = np.exp(-x**2) + 1.5 * np.exp(-(x-2)**2) + noise 
+
+    poly = PolynomialFeatures(degree=4)
+    X = poly.fit_transform(x[:, np.newaxis])
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+
+
+
+if __name__ == '__main__':
+    # exercise_34_2()
+    np.random.seed(2024)
+    exercise_34_3()
