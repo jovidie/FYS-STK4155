2870556accca7634987c88432d8752862541df32
Janita Willumsen <janitaws@icloud.com>
2024-09-04 09:34:43 +0200

    Add Ridge model

--- code/models.py
+++ code/models.py
@@ -29,9 +29,38 @@ class LinRegression:
         return self._mse, self._r2
     
 
+class RidgeRegression:
+
+    def __init__(self, degree, lmbda) -> None:
+        self._degree = degree + 1
+        self._lmbda = lmbda
+
+    def _design_matrix(self, x):
+        X = np.zeros((len(x), self._degree))
+        X[:, 0] = 1
+        for i in range(1, self._degree):
+            X[:, i] = x**i
+        return X
+    
+    def fit(self, x_train, y_train):
+        self._X = self._design_matrix(x_train)
+        self._X_T = self._X.T
+        XTX = self._X_T @ self._X
+        self._beta = np.linalg.inv(XTX + self._lmbda*np.eye(len(XTX))) @ self._X_T @ y_train 
+    
+    def predict(self, x_test):
+        X_test = self._design_matrix(x_test)
+        y_pred = X_test @ self._beta
+        return y_pred 
+    
+    def compute_error(self, y_true, y_pred):
+        self._mse = mse(y_true, y_pred)
+        self._r2 = r2(y_true, y_pred)
+        return self._mse, self._r2
+
 
 if __name__ == '__main__':
-    model = LinRegression(2)
+    model = RidgeRegression(degree=2, lmbda=0.001)
     x = np.random.rand(100)
     y = 2.0 + 5*x**2 + 0.1*np.random.randn(100)
 
