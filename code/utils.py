99bd4aec174095afad3d3d01c6d15b69ab688955
Janita Willumsen <janitaws@icloud.com>
2024-09-06 11:36:57 +0200

    Add design matrix function

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+def design_matrix(x:np.array, degree:int):
+    if degree < 1:
+        raise ValueError("Invalid value, degree have to be larger or equal to 1.")
+    X = np.column_stack((np.ones_like(x), x))
+    for i in range(2, degree+1):
+        X = np.column_stack((X, x**i))
+    return X
+
 def mse(y_true, y_pred):
     n = np.size(y_pred)
     return np.sum((y_true - y_pred)**2) / n
