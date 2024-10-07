29c8361174ad01f5514dfe87f9086d4b2b695d2f
Janita Willumsen <janitaws@icloud.com>
2024-10-07 10:12:41 +0200

    Regression models, start trainer class

--- project-1/src/pone/models.py
+++ project-1/src/pone/models.py
@@ -1,27 +1,9 @@
-from abc import ABC, abstractmethod
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Lasso
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from matplotlib import cm
 import numpy as np
-
-from pone.utils import design_matrix, franke_function
-
-class Regression(ABC):
-    """Abstract class for regression models."""
-    @abstractmethod
-    def fit(self, X_train, y_train):
-        pass
-    
-    @abstractmethod
-    def predict(self, X_test):
-        pass
+from sklearn.linear_model import Lasso
 
 
-class LinearRegression(Regression):
-
+class OLSRegression:
+    """Ordinary Least Square regression model."""
     def __init__(self) -> None:
         self._beta = None
     
@@ -33,15 +15,14 @@ class LinearRegression(Regression):
         XT = X_train.T
         XTX = XT @ X_train
         self._beta = np.linalg.pinv(XTX) @ XT @ y_train 
-        return self
 
     def predict(self, X_test):
         y_pred = X_test @ self._beta
-        return y_pred 
+        return y_pred  
 
 
-class RidgeRegression(Regression):
-
+class RidgeRegression:
+    """Regression model with Ridge regularization"""
     def __init__(self, lmbda) -> None:
         self._lmbda = lmbda
         self._beta = None
@@ -61,10 +42,10 @@ class RidgeRegression(Regression):
         return y_pred 
 
 
-class LassoRegression(Regression):
-
-    def __init__(self, lmbda) -> None:
-        self._model = Lasso(lmbda)
+class LassoRegression:
+    """Regression model with Lasso regularization, wraps sklearn model."""
+    def __init__(self, lmbda, iter=10_000) -> None:
+        self._model = Lasso(fit_intercept=False, max_iter=iter, alpha=lmbda)
         self._beta = None
 
     @property
@@ -72,12 +53,25 @@ class LassoRegression(Regression):
         return self._beta
         
     def fit(self, X_train, y_train):
-        # Needs implementation for Lasso
-        # XT = X_train.T
-        # XTX = XT @ X_train
-        # self._beta = np.linalg.pinv(XTX + self._lmbda*np.eye(len(XTX))) @ XT @ y_train 
         self._beta = self._model.fit(X_train, y_train)
-        return self
 
     def predict(self, X_test):
         return self._model.predict(X_test)
+    
+
+class Trainer:
+    """Train model"""
+    def __init__(self, P) -> None:
+        self._P = P
+
+    
+    def train_model(self, model):
+        pass 
+
+
+    def error(self):
+        pass
+
+
+    def plot(self):
+        pass
